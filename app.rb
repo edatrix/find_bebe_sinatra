5adffdba209072ee2a8913233929027ad72fef09
edatrix <lespath@gmail.com>
2014-01-19 22:01:53 -0700

    added delete, edit, and show page for edit

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 class FindBebe < Sinatra::Base
+  set :method_override, true
   configure :development do
     register Sinatra::Reloader
   end
@@ -14,6 +15,26 @@ class FindBebe < Sinatra::Base
 
   get '/' do
     erb :index, locals: {pets: Pet.all}
+  end
+
+  delete '/:id' do |id|
+    Pet.delete(id.to_i)
+    redirect '/'
+    "Deleting pet listing!"
+  end
+
+  get '/:id/edit' do |id|
+    pet = Pet.find(id.to_i)
+    erb :edit, locals: {id: id, pet: pet}
+  end
+
+  put '/:id' do |id|
+    data = {
+      :name => params['pet_name'],
+      :description => params['pet_description']
+    }
+    Pet.update(id.to_i, data)
+    redirect '/'
   end
 
   not_found do
--- pet.rb
+++ pet.rb
@@ -28,6 +28,29 @@ class Pet
     end
   end
 
+  def self.delete(position)
+    database.transaction do
+      database['pets'].delete_at(position)
+    end
+  end
+
+  def self.find(id)
+    raw_pet = find_raw_pet(id)
+    new(raw_pet[:name], raw_pet[:description])
+  end
+
+  def self.find_raw_pet(id)
+    database.transaction do
+      database['pets'].at(id)
+    end
+  end
+
+  def self.update(id, data)
+    database.transaction do
+      database['pets'][id] = data
+    end
+  end
+
   def self.database
     @database ||=YAML::Store.new("pets")
   end
